61329204e0d822693198119189d40c6b471ba858
maciejpawezka <maciej.pawezka@gmail.com>
2026-03-31 22:21:30 +0200

    testing

--- scraper.py
+++ scraper.py
@@ -22,7 +22,9 @@ RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL")
 
 def get_edel_optics_price(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+        "X-Forwarded-For": "31.0.0.0" # Losowy Polski IP z puli Orange, tylko dla niepoznaki
     }
     scraper = cloudscraper.create_scraper()
     response = scraper.get(url, headers=headers)
